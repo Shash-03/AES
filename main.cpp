effb6dcbcacb113c0ad5b9faca27e8f6f4214623
Shashwat <shashwatj0303@gmail.com>
2024-06-08 00:23:23 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h> 
+ 
+using namespace std;
+ 
+
+void solve(){
+ 
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+                 
+    int tt;
+    cin >> tt;
+    while(tt--) {
+        solve();
+    }
+    return 0;
+}
