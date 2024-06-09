ab2566131314206afc3222bde061cf62d236ac3a
Shashwat <shashwatj0303@gmail.com>
2024-06-09 16:27:00 +0530

    AES 128 done. Also cleaned up the code and added classes

--- decrypt.cpp
+++ decrypt.cpp
@@ -3,202 +3,249 @@
 
 using namespace std;
 
-//Add Round key operation
-void AddRoundKey(unsigned char cipherText[4][4],unsigned char key[4][4]){
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            cipherText[j][i] ^= key[j][i];
-        }
-    }
-}
+class AES_Operations{
+public:
 
-//Inverse sub bytes operation
-void InverseSubBytes(unsigned char cipherText[4][4]){
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            cipherText[j][i] = inverseSBox[cipherText[j][i]];
-        }
-    }
-}
-
-//Inverse shift rows operation
-void InverseShiftRows(unsigned char cipherText[4][4]){
-    unsigned char shift[4][4];
-
-    for (int i = 0; i < 4;i++){
-        for (int j = 0;j < 4;j++){
-            shift[i][j] = cipherText[i][(j-i + 4)%4];
+    //Add Round key operation
+    static void AddRoundKey(unsigned char cipherText[4][4],unsigned char key[4][4]){
+        for (int i = 0; i < 4;i++){
+            for (int j = 0; j < 4;j++){
+                cipherText[j][i] ^= key[j][i];
+            }
         }
     }
 
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            cipherText[j][i] = shift[j][i];
+    //Inverse sub bytes operation
+    static void InverseSubBytes(unsigned char cipherText[4][4]){
+        for (int i = 0; i < 4;i++){
+            for (int j = 0; j < 4;j++){
+                cipherText[j][i] = inverseSBox[cipherText[j][i]];
+            }
         }
     }
-}
 
-void KeyExpansion128(unsigned char expandedKeys[11][4][4]) {
-    for (int round = 1; round <= 10; round++) {
-        //We can simply find w5,w6,w7 by w0 ^ w1 and so on
-        for (int j = 3; j >= 1 ; j--){
+    //Inverse shift rows operation
+    static void InverseShiftRows(unsigned char cipherText[4][4]){
+        unsigned char shift[4][4];
 
-            for (int i = 0; i < 4;i++){
-                expandedKeys[round][i][j] = expandedKeys[round - 1][i][j-1] ^ expandedKeys[round - 1][i][j];
+        for (int i = 0; i < 4;i++){
+            for (int j = 0;j < 4;j++){
+                shift[i][j] = cipherText[i][(j-i + 4)%4];
             }
         }
 
-        //To find w4, we take g(w3) ^ w0
-        unsigned char col[4];
-
         for (int i = 0; i < 4;i++){
-            col[i] = expandedKeys[round - 1][i][2] ^ expandedKeys[round - 1][i][3];
+            for (int j = 0; j < 4;j++){
+                cipherText[j][i] = shift[j][i];
+            }
         }
+    }
 
+    static void InverseMixColumns(unsigned char cipherText[4][4]) {
 
-        
-        unsigned char temp = col[0];
-        for (int i = 0; i < 3; i++) {
-            col[i] = col[i + 1];
-        }
-        col[3] = temp;
+        //For the inverse mix columns, we multiply by 11x^3 + 13x^2 + 9x + 14
+        unsigned char mul[4][4];
 
         for (int i = 0; i < 4; i++) {
-            col[i] = sBox[col[i]];
+            unsigned char col[4];
+            for (int j = 0; j < 4; j++) {
+                col[j] = cipherText[j][i];
+            }
+
+            unsigned char Times_2[4], Times_4[4], Times_8[4];
+            unsigned char Times_9[4], Times_11[4], Times_13[4], Times_14[4];
+
+            for (int j = 0; j < 4; j++) {
+                if (col[j] & 0x80) {
+                    Times_2[j] = (col[j] << 1) ^ 0x1b;
+                } else {
+                    Times_2[j] = col[j] << 1;
+                }
+            }
+
+            for (int j = 0; j < 4; j++) {
+                if (Times_2[j] & 0x80) {
+                    Times_4[j] = (Times_2[j] << 1) ^ 0x1b;
+                } else {
+                    Times_4[j] = Times_2[j] << 1;
+                }
+            }
+
+            for (int j = 0; j < 4; j++) {
+                if (Times_4[j] & 0x80) {
+                    Times_8[j] = (Times_4[j] << 1) ^ 0x1b;
+                } else {
+                    Times_8[j] = Times_4[j] << 1;
+                }
+            }
+
+            for (int j = 0; j < 4; j++) {
+                Times_9[j] = Times_8[j] ^ col[j];
+                Times_11[j] = Times_8[j] ^ Times_2[j] ^ col[j];
+                Times_13[j] = Times_8[j] ^ Times_4[j] ^ col[j];
+                Times_14[j] = Times_8[j] ^ Times_4[j] ^ Times_2[j];
+            }
+
+            mul[0][i] = Times_14[0] ^ Times_11[1] ^ Times_13[2] ^ Times_9[3];
+            mul[1][i] = Times_9[0] ^ Times_14[1] ^ Times_11[2] ^ Times_13[3];
+            mul[2][i] = Times_13[0] ^ Times_9[1] ^ Times_14[2] ^ Times_11[3];
+            mul[3][i] = Times_11[0] ^ Times_13[1] ^ Times_9[2] ^ Times_14[3];
         }
 
-        col[0] ^= rc[10 - round];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                cipherText[j][i] = mul[j][i];
+            }
+        }
+    }
+
+    static void Round(unsigned char cipherMatrix[4][4],unsigned char keyMatrix[4][4] ){
+        InverseShiftRows(cipherMatrix);
+        InverseSubBytes(cipherMatrix);
+        AddRoundKey(cipherMatrix, keyMatrix);
+        InverseMixColumns(cipherMatrix);
+    }
+
+    static void LastRound(unsigned char cipherMatrix[4][4],unsigned char keyMatrix[4][4] ){
+        InverseShiftRows(cipherMatrix);
+        InverseSubBytes(cipherMatrix);
+        AddRoundKey(cipherMatrix, keyMatrix);
+    }
+
+
+};
+
+
+class AES128_Decryption{
+private:
+    char hexaCipher[33];
+    char hexaKey[33];
+
+public:
+
+    AES128_Decryption(const char *cipher, const char *key){
+        setHexaCipher(cipher);
+        setHexaKey(key);
+    }
+
+    char* getHexaCipher() {
+        return hexaCipher;
+    }
+
+   
+    void setHexaCipher(const char* value) {
+        strncpy(hexaCipher, value, 32);
+        hexaCipher[32] = '\0'; 
+    }
+
+    char* getHexaKey() {
+        return hexaKey;
+    }
+
+   
+    void setHexaKey(const char* value) {
+        strncpy(hexaKey, value, 32);
+        hexaKey[32] = '\0';
+    }
+
+
+    void KeyExpansion128(unsigned char expandedKeys[11][4][4]) {
+        for (int round = 1; round <= 10; round++) {
+            //We can simply find w5,w6,w7 by w0 ^ w1 and so on
+            for (int j = 3; j >= 1 ; j--){
+
+                for (int i = 0; i < 4;i++){
+                    expandedKeys[round][i][j] = expandedKeys[round - 1][i][j-1] ^ expandedKeys[round - 1][i][j];
+                }
+            }
+
+            //To find w4, we take g(w3) ^ w0
+            unsigned char col[4];
+
+            for (int i = 0; i < 4;i++){
+                col[i] = expandedKeys[round - 1][i][2] ^ expandedKeys[round - 1][i][3];
+            }
+            
+            unsigned char temp = col[0];
+            for (int i = 0; i < 3; i++) {
+                col[i] = col[i + 1];
+            }
+            col[3] = temp;
+
+            for (int i = 0; i < 4; i++) {
+                col[i] = sBox[col[i]];
+            }
+
+            col[0] ^= rc[10 - round];
+
+            for (int i = 0; i < 4;i++){
+                expandedKeys[round][i][0] = col[i] ^ expandedKeys[round - 1][i][0];
+            }
+
+        }
+        
+    }
+
+
+    void Decryption128(unsigned char cipherMatrix[4][4],unsigned char expandedKeys[11][4][4] ){
+        AES_Operations::AddRoundKey(cipherMatrix,expandedKeys[0]);
+
+        for (int i = 1; i <= 9;i++){
+            AES_Operations::Round(cipherMatrix,expandedKeys[i]);
+        }
+
+        AES_Operations::LastRound(cipherMatrix,expandedKeys[10]);
+    }
+
+    void Decrypt(){
+
+        unsigned char cipherText[16];
+        unsigned char cipherMatrix[4][4];
+
+        unsigned char key[16];
+        unsigned char keyMatrix[4][4];
+
+        char* hexaCipher = getHexaCipher();
+        char* hexaKey = getHexaKey();
+
+        HexaArrayToByteArray(cipherText, hexaCipher);
+        ByteArrayToByteMatrix(cipherMatrix,cipherText);
+
+        HexaArrayToByteArray(key, hexaKey);
+        ByteArrayToByteMatrix(keyMatrix,key);
+
+        unsigned char expandedKeys[11][4][4];
 
         for (int i = 0; i < 4;i++){
-            expandedKeys[round][i][0] = col[i] ^ expandedKeys[round - 1][i][0];
-        }
-
-    }
-    
-}
-
-void InverseMixColumns(unsigned char cipherText[4][4]) {
-
-    //For the inverse mix columns, we multiply by 11x^3 + 13x^2 + 9x + 14
-    unsigned char mul[4][4];
-
-    for (int i = 0; i < 4; i++) {
-        unsigned char col[4];
-        for (int j = 0; j < 4; j++) {
-            col[j] = cipherText[j][i];
-        }
-
-        unsigned char Times_2[4], Times_4[4], Times_8[4];
-        unsigned char Times_9[4], Times_11[4], Times_13[4], Times_14[4];
-
-        for (int j = 0; j < 4; j++) {
-            if (col[j] & 0x80) {
-                Times_2[j] = (col[j] << 1) ^ 0x1b;
-            } else {
-                Times_2[j] = col[j] << 1;
+            for (int j = 0; j < 4;j++){
+                expandedKeys[0][i][j] = keyMatrix[i][j];
             }
         }
 
-        for (int j = 0; j < 4; j++) {
-            if (Times_2[j] & 0x80) {
-                Times_4[j] = (Times_2[j] << 1) ^ 0x1b;
-            } else {
-                Times_4[j] = Times_2[j] << 1;
-            }
+        KeyExpansion128(expandedKeys);
+
+        Decryption128(cipherMatrix,expandedKeys);
+
+        ByteMatrixToByteArray(cipherMatrix,cipherText);
+        ByteArrayToHexaArray(cipherText,hexaCipher);
+
+        for (int i = 0; i < 32;i++){
+            cout << hexaCipher[i] ;
         }
 
-        for (int j = 0; j < 4; j++) {
-            if (Times_4[j] & 0x80) {
-                Times_8[j] = (Times_4[j] << 1) ^ 0x1b;
-            } else {
-                Times_8[j] = Times_4[j] << 1;
-            }
-        }
-
-        for (int j = 0; j < 4; j++) {
-            Times_9[j] = Times_8[j] ^ col[j];
-            Times_11[j] = Times_8[j] ^ Times_2[j] ^ col[j];
-            Times_13[j] = Times_8[j] ^ Times_4[j] ^ col[j];
-            Times_14[j] = Times_8[j] ^ Times_4[j] ^ Times_2[j];
-        }
-
-        mul[0][i] = Times_14[0] ^ Times_11[1] ^ Times_13[2] ^ Times_9[3];
-        mul[1][i] = Times_9[0] ^ Times_14[1] ^ Times_11[2] ^ Times_13[3];
-        mul[2][i] = Times_13[0] ^ Times_9[1] ^ Times_14[2] ^ Times_11[3];
-        mul[3][i] = Times_11[0] ^ Times_13[1] ^ Times_9[2] ^ Times_14[3];
     }
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            cipherText[j][i] = mul[j][i];
-        }
-    }
-}
-
-void Round(unsigned char cipherMatrix[4][4],unsigned char keyMatrix[4][4] ){
-    InverseShiftRows(cipherMatrix);
-    InverseSubBytes(cipherMatrix);
-    AddRoundKey(cipherMatrix, keyMatrix);
-    InverseMixColumns(cipherMatrix);
-}
-
-void LastRound(unsigned char cipherMatrix[4][4],unsigned char keyMatrix[4][4] ){
-    InverseShiftRows(cipherMatrix);
-    InverseSubBytes(cipherMatrix);
-    AddRoundKey(cipherMatrix, keyMatrix);
-}
-
-void AESDecryption128(unsigned char cipherMatrix[4][4],unsigned char expandedKeys[11][4][4] ){
-    AddRoundKey(cipherMatrix,expandedKeys[0]);
-
-    for (int i = 1; i <= 9;i++){
-        Round(cipherMatrix,expandedKeys[i]);
-    }
-
-    LastRound(cipherMatrix,expandedKeys[10]);
-}
-
-
-
-
-
+};
 
 
 int main(){
+    const char* hexaCipher = "69c4e0d86a7b0430d8cdb78070b4c55a";
+    const char* hexaKey = "13111d7fe3944a17f307a78b4d2b30c5";
 
-    unsigned char cipherText[16];
-    char hexaCipher[33] = "69c4e0d86a7b0430d8cdb78070b4c55a";
-    unsigned char cipherMatrix[4][4];
+    AES128_Decryption aes128(hexaCipher,hexaKey);
 
-    unsigned char key[16];
-    unsigned char keyMatrix[4][4];
-    char hexaKey[33] = "13111d7fe3944a17f307a78b4d2b30c5";
+    aes128.Decrypt();
 
-    HexaArrayToByteArray(cipherText, hexaCipher);
-    ByteArrayToByteMatrix(cipherMatrix,cipherText);
-
-    HexaArrayToByteArray(key, hexaKey);
-    ByteArrayToByteMatrix(keyMatrix,key);
-
-    unsigned char expandedKeys[11][4][4];
-
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            expandedKeys[0][i][j] = keyMatrix[i][j];
-        }
-    }
-
-    KeyExpansion128(expandedKeys);
-
-    AESDecryption128(cipherMatrix,expandedKeys);
-
-
-    ByteMatrixToByteArray(cipherMatrix,cipherText);
-    ByteArrayToHexaArray(cipherText,hexaCipher);
-
-    for (int i = 0; i < 32;i++){
-        cout << hexaCipher[i] ;
-    }
 
     return 0;
 }
--- encrypt.cpp
+++ encrypt.cpp
@@ -3,200 +3,243 @@
 
 using namespace std;
 
+class AES_Operations {
+public:
 
-
-
-//Add Round key operation
-void AddRoundKey(unsigned char primaryText[4][4],unsigned char key[4][4]){
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            primaryText[j][i] ^= key[j][i];
+    // Add Round Key operation
+    static void AddRoundKey(unsigned char primaryText[4][4], unsigned char key[4][4]) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                primaryText[j][i] ^= key[j][i];
+            }
         }
     }
 
-}
-
-//Sub Bytes operation
-void SubBytes(unsigned char primaryText[4][4]){
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            primaryText[j][i] = sBox[primaryText[j][i]];
+    // Sub Bytes operation
+    static void SubBytes(unsigned char primaryText[4][4]) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                primaryText[j][i] = sBox[primaryText[j][i]];
+            }
         }
     }
 
-}
+    // Shift Rows
+    static void ShiftRows(unsigned char primaryText[4][4]) {
+        unsigned char shift[4][4];
 
-//Shift Rows
-void ShiftRows(unsigned char primaryText[4][4]){
-    unsigned char shift[4][4];
-
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            shift[i][j] = primaryText[i][(j+i)%4]; // The value in (i,j) would be the value in the original matrix
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                shift[i][j] = primaryText[i][(j + i) % 4];// The value in (i,j) would be the value in the original matrix
                                                     // which after left shifting by i would land on (i,j)
-        }
-    }
-
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            primaryText[j][i] = shift[j][i];
-        }
-    }
-
-}
-
-//Mix Column Operation
-void MixColumns(unsigned char primaryText[4][4]){
-    unsigned char mul[4][4];
-
-    for (int i = 0; i < 4; i++){
-        unsigned char col[4];
-        for (int j = 0; j < 4; j++){
-            col[j] = primaryText[j][i];
-        }
-
-        unsigned char Times_2[4];
-        for (int j = 0; j < 4; j++){
-            if (col[j] & 0x80) { // If the highest bit is set, it will overflow
-                Times_2[j] = (col[j] << 1) ^ 0x1b; // Left shift and XOR with 0x1b(Taking modulo x**8 + x** 4 + x**3 + x + 1)
-            } else {
-                Times_2[j] = col[j] << 1; // Just left shift
             }
         }
 
-        unsigned char Times_3[4];
-        for (int j = 0; j < 4; j++){
-            Times_3[j] = Times_2[j] ^ col[j]; // Multiplying by 3 is equivalent to (2 * value) ^ value
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                primaryText[j][i] = shift[j][i];
+            }
         }
-        /* The mix column operation involves the multiplication of 
-        each column by a matrix*/
-        /* The values of the matrix can be derived by multiplying 
-        the polynomial 3x^3 + x^2 + x + 2 by the polynomial represented
-        by a column modulo (x^4 + 1) */
-        //Over GF(2**8), x^i mod (x^4 + 1) = x^(i mod 4)
-        mul[0][i] = Times_2[0] ^ Times_3[1] ^ col[2] ^ col[3];
-        mul[1][i] = col[0] ^ Times_2[1] ^ Times_3[2] ^ col[3];
-        mul[2][i] = col[0] ^ col[1] ^ Times_2[2] ^ Times_3[3];
-        mul[3][i] = Times_3[0] ^ col[1] ^ col[2] ^ Times_2[3];
     }
 
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-            primaryText[j][i] = mul[j][i];
+    // Mix Column Operation
+    static void MixColumns(unsigned char primaryText[4][4]) {
+        unsigned char mul[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            unsigned char col[4];
+            for (int j = 0; j < 4; j++) {
+                col[j] = primaryText[j][i];
+            }
+
+            unsigned char Times_2[4];
+            for (int j = 0; j < 4; j++) {
+                if (col[j] & 0x80) {// If the highest bit is set, it will overflow
+                    Times_2[j] = (col[j] << 1) ^ 0x1b; // Left shift and XOR with 0x1b(Taking modulo x**8 + x** 4 + x**3 + x + 1)  
+                } 
+                else {
+                    Times_2[j] = col[j] << 1;
+                }
+            }
+
+            unsigned char Times_3[4];
+            for (int j = 0; j < 4; j++) {
+                Times_3[j] = Times_2[j] ^ col[j];
+            }
+
+            /* The mix column operation involves the multiplication of 
+            each column by a matrix*/
+            /* The values of the matrix can be derived by multiplying 
+            the polynomial 3x^3 + x^2 + x + 2 by the polynomial represented
+            by a column modulo (x^4 + 1) */
+            //Over GF(2**8), x^i mod (x^4 + 1) = x^(i mod 4)
+
+            mul[0][i] = Times_2[0] ^ Times_3[1] ^ col[2] ^ col[3];
+            mul[1][i] = col[0] ^ Times_2[1] ^ Times_3[2] ^ col[3];
+            mul[2][i] = col[0] ^ col[1] ^ Times_2[2] ^ Times_3[3];
+            mul[3][i] = Times_3[0] ^ col[1] ^ col[2] ^ Times_2[3];
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                primaryText[j][i] = mul[j][i];
+            }
         }
     }
-}
 
+    // Round function
+    static void Round(unsigned char primaryText[4][4], unsigned char key[4][4]) {
+        SubBytes(primaryText);
+        ShiftRows(primaryText);
+        MixColumns(primaryText);
+        AddRoundKey(primaryText, key);
+    }
 
-//Round function
-void Round(unsigned char primaryText[4][4],unsigned char key[4][4]){
-    SubBytes(primaryText);
-    ShiftRows(primaryText);
-    MixColumns(primaryText);
-    AddRoundKey(primaryText,key);
-}
+    // Last round function without the mix columns operation
+    static void LastRound(unsigned char primaryText[4][4], unsigned char key[4][4]) {
+        SubBytes(primaryText);
+        ShiftRows(primaryText);
+        AddRoundKey(primaryText, key);
+    }
+};
 
-//Round function for the last round without the mix columns operation
-void LastRound(unsigned char primaryText[4][4],unsigned char key[4][4]){
-    SubBytes(primaryText);
-    ShiftRows(primaryText);
-    AddRoundKey(primaryText,key);  
-}
+class AES128_Encryption {
+private:
+    char hexaPrimary[33];
+    char hexaKey[33];
 
-void KeyExpansion128(unsigned char expandedKeys[11][4][4]) {
-    for (int round = 1; round <= 10; round++) {
-        unsigned char col[4];
+public:
+    
+    AES128_Encryption(const char* primary,const char* key) {
+        setHexaPrimary(primary);
+        setHexaKey(key);
+    }
 
-        // Get the last column from the previous round key
-        for (int i = 0; i < 4; i++) {
-            col[i] = expandedKeys[round - 1][i][3];
-        }
+    
+    char* getHexaPrimary() {
+        return hexaPrimary;
+    }
 
-        // Perform the key schedule core (rotWord and subWord)
-        unsigned char temp = col[0];
-        for (int i = 0; i < 3; i++) {
-            col[i] = col[i + 1];
-        }
-        col[3] = temp;
+   
+    void setHexaPrimary(const char* value) {
+        strncpy(hexaPrimary, value, 32);
+        hexaPrimary[32] = '\0'; 
+    }
 
-        for (int i = 0; i < 4; i++) {
-            col[i] = sBox[col[i]];
-        }
+    char* getHexaKey() {
+        return hexaKey;
+    }
 
-        col[0] ^= rc[round - 1];
+   
+    void setHexaKey(const char* value) {
+        strncpy(hexaKey, value, 32);
+        hexaKey[32] = '\0';
+    }
 
-        // Compute the first column of the new round key
-        for (int i = 0; i < 4; i++) {
-            expandedKeys[round][i][0] = expandedKeys[round - 1][i][0] ^ col[i];
-        }
+    void KeyExpansion128(unsigned char expandedKeys[11][4][4]) {
+        for (int round = 1; round <= 10; round++) {
+            unsigned char col[4];
 
-        // Compute the rest of the columns of the new round key
-        for (int j = 1; j < 4; j++) {
+            // Get the last column from the previous round key
             for (int i = 0; i < 4; i++) {
-                expandedKeys[round][i][j] = expandedKeys[round][i][j - 1] ^ expandedKeys[round - 1][i][j];
+                col[i] = expandedKeys[round - 1][i][3];
+            }
+
+            // Perform the key schedule core (rotWord and subWord)
+            unsigned char temp = col[0];
+            for (int i = 0; i < 3; i++) {
+                col[i] = col[i + 1];
+            }
+            col[3] = temp;
+
+            for (int i = 0; i < 4; i++) {
+                col[i] = sBox[col[i]];
+            }
+
+            col[0] ^= rc[round - 1];
+
+            // Compute the first column of the new round key
+            for (int i = 0; i < 4; i++) {
+                expandedKeys[round][i][0] = expandedKeys[round - 1][i][0] ^ col[i];
+            }
+
+            // Compute the rest of the columns of the new round key
+            for (int j = 1; j < 4; j++) {
+                for (int i = 0; i < 4; i++) {
+                    expandedKeys[round][i][j] = expandedKeys[round][i][j - 1] ^ expandedKeys[round - 1][i][j];
+                }
             }
         }
     }
-}
 
 
+    void Encryption128(unsigned char primaryMatrix[4][4], unsigned char expandedKeys[11][4][4]){
+        AES_Operations::AddRoundKey(primaryMatrix,expandedKeys[0]);
 
-void AESEncryption128(unsigned char primaryMatrix[4][4], unsigned char expandedKeys[11][4][4]){
-    AddRoundKey(primaryMatrix,expandedKeys[0]);
+        for (int i = 1; i <= 9;i++){
+            AES_Operations::Round(primaryMatrix,expandedKeys[i]);
+        }
 
-    for (int i = 1; i <= 9;i++){
-        Round(primaryMatrix,expandedKeys[i]);
+        AES_Operations::LastRound(primaryMatrix,expandedKeys[10]);
     }
 
-    LastRound(primaryMatrix,expandedKeys[10]);
-}
+    void Encrypt() {
+        unsigned char primaryText[16];
+        unsigned char primaryMatrix[4][4];
+
+        unsigned char key[16];
+        unsigned char keyMatrix[4][4];
+        
+        // Get hexaPrimary and hexaKey values
+        char* hexaPrimary = getHexaPrimary();
+        char* hexaKey = getHexaKey();
+
+        
+        HexaArrayToByteArray(primaryText, hexaPrimary);
+        ByteArrayToByteMatrix(primaryMatrix, primaryText);
+
+        HexaArrayToByteArray(key, hexaKey);
+        ByteArrayToByteMatrix(keyMatrix, key);
+        
+        unsigned char expandedKeys[11][4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                expandedKeys[0][i][j] = keyMatrix[i][j];
+            }
+        }
+
+        KeyExpansion128(expandedKeys);
+
+        Encryption128(primaryMatrix, expandedKeys);
+
+        ByteMatrixToByteArray(primaryMatrix, primaryText);
+        ByteArrayToHexaArray(primaryText, hexaPrimary);
+        
+        for (int i = 0; i < 32; i++) {
+            cout << hexaPrimary[i];
+        }
+        cout << endl;
+    }
+};
+
+
+
+
+
 
 
 
 int main(){
 
-    unsigned char primaryText[16];
-    char hexaPrimary[33] = "00112233445566778899aabbccddeeff";
-    unsigned char primaryMatrix[4][4];
+    const char* hexaPrimary = "00112233445566778899aabbccddeeff";
 
-    unsigned char key[16];
-    unsigned char keyMatrix[4][4];
-    char hexaKey[33] = "000102030405060708090a0b0c0d0e0f";
+    const char* hexaKey = "000102030405060708090a0b0c0d0e0f";
 
-    HexaArrayToByteArray(primaryText, hexaPrimary);
-    ByteArrayToByteMatrix(primaryMatrix,primaryText);
+    AES128_Encryption aes128(hexaPrimary,hexaKey);
 
-    HexaArrayToByteArray(key, hexaKey);
-    ByteArrayToByteMatrix(keyMatrix,key);
-    
-    unsigned char expandedKeys[11][4][4];
+    aes128.Encrypt();
 
-    for (int i = 0; i < 4;i++){
-        for (int j = 0; j < 4;j++){
-            expandedKeys[0][i][j] = keyMatrix[i][j];
-        }
-    }
-
-    KeyExpansion128(expandedKeys);
-
-    AESEncryption128(primaryMatrix,expandedKeys);
-
-    ByteMatrixToByteArray(primaryMatrix,primaryText);
-    ByteArrayToHexaArray(primaryText,hexaPrimary);
-
-    ByteMatrixToByteArray(expandedKeys[9],key);
-    ByteArrayToHexaArray(key,hexaKey);
-
-    for (int i = 0; i < 32;i++){
-        cout << hexaPrimary[i] ;
-    }
-    
-
-    cout << "\n";
-
-    for (int i = 0; i < 32;i++){
-        cout << hexaKey[i];
-    }
 
     return 0;
 }
